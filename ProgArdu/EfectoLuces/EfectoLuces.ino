d76345d859eca0447c90ffd2ef4ba0f01f045537
alfredo@xiverty.com <alfredo>
2015-06-01 02:04:26 -0400

    agragando un proyecto en arduino

---
+++ ProgArdu/EfectoLuces/EfectoLuces.ino
@@ -0,0 +1,39 @@
+
+int led;
+
+ void setup(){
+   for(int i=4;i<=13;i++){
+     led=i;
+     pinMode(led,OUTPUT);
+   }
+ }
+ 
+  void loop(){
+    uno();
+    dos();
+  }
+  
+  void uno(){
+  for(int i=4;i<=13;i++){
+    led=i;
+    digitalWrite(led,HIGH);
+    delay(20);
+    digitalWrite(led,LOW);
+    delay(20);
+  }
+  }
+  
+    void dos(){
+    for(int i=13;i>=4;i--){
+    led=i;
+    digitalWrite(led,HIGH);
+    delay(20);
+    digitalWrite(led,LOW);
+    delay(20);
+  }
+  }
+  
+ 
+ 
+ 
+ 
